e8124ce2ced67f0b7154d681907132120f7d7e0d
Atticus Wong <atticuswong@gmail.com>
2025-01-30 23:20:56 -0800

    fix: table column and feature abstractions + shadcn (#9)

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CustomerData } from "@/src/utils/types";
 import { Suspense } from "react";
 import DataTable from "@/src/components/DataTable";
 import TestTable from "@/src/components/testTable";
+import { createColumnHelper, SortingState } from "@tanstack/react-table";
+import CellInput from "@/src/components/CellInput";
+import SourceStatus from "@/src/components/SourceChip";
+import DestinationStatus from "@/src/components/DestinationChip";
+import PaymentFrequencyStatus from "@/src/components/PaymentFrequencyChip";
 function LoadingTable() {
   return <div>Loading...</div>;
 }
@@ -12,6 +17,81 @@ function LoadingTable() {
 function ErrorTable({ error }: { error: string }) {
   return <div>Error: {error}</div>;
 }
+
+const columnHelper = createColumnHelper<CustomerData>();
+
+const columns = [
+  columnHelper.accessor('customerName', {
+    header: 'Customer Name',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true,
+    minSize: 200,
+  }),
+  columnHelper.accessor('customerID', {
+    header: 'Customer ID',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true,
+    minSize: 200,
+  }),
+  columnHelper.accessor('opportunityName', {
+    header: 'Opportunity Name',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true,
+    minSize: 350
+  }),
+  columnHelper.accessor('type', {
+    header: 'Type',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true
+  }),
+  columnHelper.accessor('opportunityOwner', {
+    header: 'Opportunity Owner',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true
+  }),
+  columnHelper.accessor('source', {
+    header: 'Source',
+    cell: info => <SourceStatus defaultSource={info.getValue()} />,
+    enableResizing: true
+  }),
+  columnHelper.accessor('destination', {
+    header: 'Destination',
+    cell: info => <DestinationStatus defaultDestination={info.getValue()} />,
+    enableResizing: true
+  }),
+  columnHelper.accessor('formula', {
+    header: 'Formula',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true
+  }),
+  columnHelper.accessor('billingEmails', {
+    header: 'Billing Emails',
+    cell: info => <CellInput defaultValue={info.getValue().join(', ')} />,
+    enableResizing: true,
+    minSize: 500
+  }),
+  columnHelper.accessor('billingAddress', {
+    header: 'Billing Address',
+    cell: info => {
+      const address = info.getValue();
+      return <CellInput defaultValue={`${address.address}, ${address.city}, ${address.state} ${address.zip}`} />;
+    },
+    enableResizing: true,
+    minSize: 500
+  }),
+  columnHelper.accessor('paymentFrequency', {
+    header: 'Payment Frequency',
+    cell: info => <PaymentFrequencyStatus defaultFreq={info.getValue()} />,
+    enableResizing: true
+  }),
+  columnHelper.accessor('netTerms', {
+    header: 'Net Terms',
+    cell: info => <CellInput defaultValue={info.getValue()} />,
+    enableResizing: true
+  }),
+];
+
+
 
 function Table() {
   const [data, setData] = useState<CustomerData[]>([]);
@@ -40,7 +120,12 @@ function Table() {
   if (error) return <ErrorTable error={error} />;
   if (isLoading) return <LoadingTable />;
 
-  return <DataTable initialData={data} />;
+  return <DataTable 
+    initialData={data} 
+    columns={columns} 
+    enableSorting={true}
+    enableGlobalFiltering={true}
+  />;
 }
 
 export default function TableWrapper() {
